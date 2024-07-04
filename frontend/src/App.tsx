21ad8226f92d268c672def9c45fd675a2b8b35b2
lachlanshoesmith <12870244+lachlanshoesmith@users.noreply.github.com>
2024-07-05 02:32:30 +1000

    🎨 add basic landing page for demo purpose

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import '@fontsource/hammersmith-one';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app">
+      <img className="icon" src="/yellowshirt.svg" alt="yellowshirt" />
+      <h1>
+        <span className="yellow">yellow</span>
+        <span className="shirt">shirt</span>
+      </h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
