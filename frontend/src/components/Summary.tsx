5973fb57a032651820f3228e417af2c56154e83c
Elvene <xleonx0x@users.noreply.github.com>
2024-07-25 18:53:29 +1000

    created basic files and added types

---
+++ frontend/src/components/Summary.tsx
@@ -0,0 +1,19 @@
+import classes from './Summary.module.css'
+
+type summary = {
+	correctGuesses: number;
+	correctBuilding: number;
+	timeBonus: number;
+	shirtsAcquried: number;
+}
+
+function Summary(props: summary) {
+
+	return(
+		<div>
+			
+		</div>
+	)
+}
+
+export default Summary
