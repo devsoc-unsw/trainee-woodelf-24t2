3907bda752b894e1430296c0d89fa5dd9fc1fa77
Elvene <123799877+xleonx0x@users.noreply.github.com>
2024-07-25 20:59:43 +1000

    made progress on summary component

--- frontend/src/components/Summary.tsx
+++ frontend/src/components/Summary.tsx
@@ -1,21 +1,41 @@
 import classes from './Summary.module.css'
 import { useEffect } from 'react'
 
-interface summary {
+interface Summary {
 	correctGuesses: number;
 	correctBuilding: number;
 	timeBonus: number;
 	shirtsAcquried: number;
 }
 
-function Summary(props: summary) {
+function Summary(props: Summary) {
 
 	return(
-			<div className={classes.container}>
-				<div className={classes.table}>
-
-				</div>
+		<div className={classes.container}>
+			<div className={classes.title}>
+				Summary
 			</div>
+				<hr></hr>
+			<div className={classes.table}>
+				<ul className={classes.fields}>
+					<li>
+						<span className='light-text'>✅ Correct Guesses:</span> {props.correctGuesses}
+					</li>
+					<li>
+						<span className='light-text'>🏫 Correct Building:</span> {props.correctBuilding}
+					</li>
+					<li>
+						<span className='light-text'>⏰ Time Bonus:</span> {props.timeBonus}
+					</li>
+				</ul>
+			</div>
+			<div className={classes.result}>
+			💃🥳	{props.shirtsAcquried} Shirts Acquired 💃🥳
+			</div>
+			<button className={classes.button}>
+				Good job!
+			</button>
+		</div>
 	)
 }
 
