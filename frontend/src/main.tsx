5267d845779ab6f39b1a95fefbcc52256c9a12f3
lachlanshoesmith <12870244+lachlanshoesmith@users.noreply.github.com>
2024-07-25 15:39:30 +1000

    ↗ add primitive routing

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  useParams,
+} from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <div>error</div>,
+    children: [
+      {
+        path: '/play',
+        element: <div>Playing the game</div>,
+      },
+      {
+        path: '/profile/:id',
+        element: <div>Profile person</div>,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
