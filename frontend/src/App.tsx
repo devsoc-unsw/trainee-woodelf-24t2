45248a1c216d025c73aab850369432a6be3635c8
Elvene <xleonx0x@users.noreply.github.com>
2024-08-08 17:57:26 +1000

    main background added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,87 +1,9 @@
 import "@fontsource/hammersmith-one";
 import "./App.scss";
-import { useState } from "react";
-import Logo from "./components/Logo/Logo";
-
-const subscribe_shirt_fan = async (email: string, colour: string) => {
-  try {
-    await fetch(`${import.meta.env.VITE_BACKEND_URL}/subscribe`, {
-      method: "POST",
-      body: JSON.stringify({
-        email: email,
-        colour: colour,
-      }),
-      headers: { "Content-Type": "application/json" },
-    });
-    alert("thanks, a shirt is on the way");
-  } catch (e) {
-    console.error("oh no!! " + e);
-  }
-};
-
-const check_colour = async (email: string) => {
-  const url =
-    `${import.meta.env.VITE_BACKEND_URL}/colour?` +
-    new URLSearchParams({
-      email: email,
-    });
-  const response = await fetch(url, {
-    method: "GET",
-    headers: {
-      "Content-type": "application/json",
-    },
-  });
-  if (response.status === 200) {
-    const color = await response.text();
-    alert("i think you wanted " + color);
-  } else {
-    alert("sorry could not find your email");
-  }
-};
+import HomePage from "./components/HomePage/HomePage";
 
 function App() {
-  const [email, setEmail] = useState("");
-  const [checkEmail, setCheckEmail] = useState("");
-  const [colour, setColour] = useState("");
-
-  return (
-    <div className="app">
-      <Logo size="lg" />
-      <div style={{ marginLeft: "40px" }}>
-        <p>Enter your email for a free shirt</p>
-        <input
-          type="email"
-          name="my_email"
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Type your email here"
-        />
-        <br />
-        <input
-          type="text"
-          name="choose_colour"
-          onChange={(e) => setColour(e.target.value)}
-          placeholder="What colour shirt?"
-        />
-        <br />
-        <button onClick={() => subscribe_shirt_fan(email, colour)}>
-          sign me up bro
-        </button>
-      </div>
-      <div style={{ marginLeft: "40px" }}>
-        <p>Forgot which colour you ordered?</p>
-        <input
-          type="email"
-          name="check_email"
-          onChange={(e) => setCheckEmail(e.target.value)}
-          placeholder="Type your email here"
-        />
-        <br />
-        <button onClick={() => check_colour(checkEmail)}>
-          what was it again
-        </button>
-      </div>
-    </div>
-  );
+  return <HomePage />;
 }
 
 export default App;
---
+++ frontend/src/components/HomePage/HomePage.tsx
@@ -0,0 +1,39 @@
+// @ts-ignore
+import ReactPannellum from "react-pannellum";
+import panoramaImage from "./testimage.png";
+
+function HomePage() {
+  const config = {
+    type: "equirectangular",
+    autoLoad: true,
+    showControls: false,
+    mouseZoom: false,
+    draggable: false,
+    keyboardZoom: false,
+    showZoomCrtl: false,
+    showLoadButton: false,
+    autoRotate: 5,
+  };
+
+  const style = {
+    width: "110vw",
+    height: "110vh",
+    filter: "blur(12px)",
+    left: "-5vw",
+    top: "-5vw",
+  };
+
+  return (
+    <div>
+      <ReactPannellum
+        id="1"
+        sceneId="firstScene"
+        style={style}
+        imageSource={panoramaImage}
+        config={config}
+      />
+    </div>
+  );
+}
+
+export default HomePage;
