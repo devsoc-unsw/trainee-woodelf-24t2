773b37693905321350c0bf733e5eb4c0525e0289
Osvaldo Prajitno <z5473276@ad.unsw.edu.au>
2024-08-12 21:53:04 +1000

    changed enum values, added id property and changed time-related properties to type Date

--- backend/src/interfaces.ts
+++ backend/src/interfaces.ts
@@ -1,36 +1,31 @@
 export enum GameState {
-  IN_PROGRESS = 'in_progress',
-  FINISHED = 'finished'
-}
-
-// for destination, I'm assuming it will be a link
-// I'll change it if needed
-export interface ButtonCoordinates {
-  x: number;
-  y: number;
-  destination: string;
+  IN_PROGRESS = 0,
+  FINISHED = 1
 }
 
 export interface Level {
+  id: string;
   photoLink: string;
   locationName: string;
-  locationZCoordinate: number;
-  buttons?: ButtonCoordinates[];
+  xCoordinate: number;
+  yCoordinate: number;
+  zCoordinate: number;
 }
 
 export interface Game {
+  id: string;
   status: GameState;
   levels: Level[];
   score: number;
   owner: string;
-  startTime: number; // To calculate time bonuses?
+  startTime: Date; // To calculate time bonuses?
 }
 
 export interface User {
+  id: string;
   username: string;
-  password: string;
   highScore?: number;
   cumulativeScore?: number;
   shirts?: number;   // Would this refer to the number of secrets(clothes drawing) found?
-  dateJoined?: number; 
+  dateJoined?: Date; 
 }
