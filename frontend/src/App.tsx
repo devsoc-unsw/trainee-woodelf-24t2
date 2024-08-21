51e78f0df47bfff17011c151a04ce2d9bfcfd14d
alyssacheong <106944368+alyssacheong@users.noreply.github.com>
2024-08-21 22:14:02 +1000

    commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,47 +5,11 @@ import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="app">
-      <img className="icon" src="/yellowshirt.svg" alt="yellowshirt" />
-      <h1>
-        <span className="yellow">yellow</span>
-        <span className="shirt">shirt</span>
-      </h1>
-      <div style={{ marginLeft: '40px' }}>
-        <p>Enter your email for a free shirt</p>
-        <input
-          type="email"
-          name="my_email"
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Type your email here"
-        />
-        <br />
-        <input
-          type="text"
-          name="choose_colour"
-          onChange={(e) => setColour(e.target.value)}
-          placeholder="What colour shirt?"
-        />
-        <br />
-        <button onClick={() => subscribe_shirt_fan(email, colour)}>
-          sign me up bro
-        </button>
-      </div>
-      <div style={{ marginLeft: '40px' }}>
-        <p>Forgot which colour you ordered?</p>
-        <input
-          type="email"
-          name="check_email"
-          onChange={(e) => setCheckEmail(e.target.value)}
-          placeholder="Type your email here"
-        />
-        <br />
-        <button onClick={() => check_colour(checkEmail)}>
-          what was it again
-        </button>
-      </div>
-    </div>
-  );
+    <>
+      <Navbar />
+      <HomePage/>
+    </>
+);
 }
 
 export default App;
