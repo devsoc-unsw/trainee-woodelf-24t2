203d0547cf05cb57da8e4994a339e670fb642708
Elvene <123799877+xleonx0x@users.noreply.github.com>
2024-08-26 03:01:04 +1000

    removed code that was for teaching us backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,8 +12,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("yellowshirt backend says hello");
+app.listen(EXPRESS_PORT, () => {
+  console.log(
+    `👕💛 yellowshirt backend listening on port ${EXPRESS_PORT} 💛👕`,
+  );
 });
 
 app.post("/register", async (req: TypedRequest<LoginBody>, res: Response) => {
@@ -53,42 +55,6 @@ app.post("/login", async (req: TypedRequest<LoginBody>, res: Response) => {
       ? res.status(201).send("User Successfully Logged In")
       : res.status(401).send("Incorrect password");
   });
-});
-
-app.post("/subscribe", async (req, res) => {
-  const { email, colour } = req.body;
-  if (!email || !colour) {
-    res.status(400).send("email and colour not specified in request");
-    return;
-  }
-  const docRef = await addDoc(collection(db, "shirt_cravers"), {
-    email,
-    colour,
-  });
-  console.log("Subscribed! Doc written: ", docRef.id);
-  res.status(200).send(docRef);
-});
-
-app.get("/colour", async (req, res) => {
-  if (!req.query.email) {
-    res.status(400).send("request must specify an email");
-    return;
-  }
-  const querySnapshot = await getDocs(collection(db, "shirt_cravers"));
-  let email: string = "";
-  querySnapshot.forEach((doc) => {
-    if (doc.data().email === req.query.email) {
-      email = doc.data().colour;
-    }
-  });
-  if (email) res.status(200).send(email);
-  else res.status(404).send("this email does not have a color :(");
-});
-
-app.listen(EXPRESS_PORT, () => {
-  console.log(
-    `👕💛 yellowshirt backend listening on port ${EXPRESS_PORT} 💛👕`,
-  );
 });
 
 // curl -H 'Content-Type: application/json' -d '{ "email": "ben", "color": "pink"}' -X POST http://localhost:3000/subscribe
