1ba2066b0bef7a775b2036218314451f558ebec0
Elvene <123799877+xleonx0x@users.noreply.github.com>
2024-08-26 22:59:00 +1000

    added salting locally first

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,10 @@
 import express, { Request, Response } from "express";
-import { TypedRequest, LoginBody } from "./requestTypes";
+import { TypedRequest, LoginBody, RegisterBody } from "./requestTypes";
 import bcrypt from "bcrypt";
 import { collection, addDoc, getDocs, where, query } from "firebase/firestore";
 import { db } from "./firebase";
 import cors from "cors";
+import crypto from "crypto";
 
 const EXPRESS_PORT = 3000;
 
@@ -18,23 +19,33 @@ app.listen(EXPRESS_PORT, () => {
   );
 });
 
-app.post("/register", async (req: TypedRequest<LoginBody>, res: Response) => {
-  const { username, password } = req.body;
+app.post(
+  "/register",
+  async (req: TypedRequest<RegisterBody>, res: Response) => {
+    const { username, password } = req.body;
 
-  const querySnapshot = await getDocs(collection(db, "users"));
-  if (querySnapshot.docs.some((doc) => doc.data().username === username)) {
-    return res.status(400).send("Username already exists");
-  }
+    const querySnapshot = await getDocs(collection(db, "users"));
+    if (querySnapshot.docs.some((doc) => doc.data().username === username)) {
+      return res.status(400).send("Username already exists");
+    }
 
-  const saltRounds: number = 10;
-  const hashedPassword: string = await bcrypt.hash(password, saltRounds);
-  await addDoc(collection(db, "users"), {
-    username,
-    password: hashedPassword,
-  });
+    const salt: string = crypto.randomBytes(128).toString("base64");
+    const saltedPassword: string = password.concat(salt);
 
-  res.status(201).send("User Successfully Registered");
-});
+    const saltRounds: number = 10;
+    const hashedPassword: string = await bcrypt.hash(
+      saltedPassword,
+      saltRounds,
+    );
+    await addDoc(collection(db, "users"), {
+      username,
+      password: hashedPassword,
+      salt: salt,
+    });
+
+    res.status(201).send("User Successfully Registered");
+  },
+);
 
 app.post("/login", async (req: TypedRequest<LoginBody>, res: Response) => {
   const { username, password } = req.body;
@@ -45,16 +56,23 @@ app.post("/login", async (req: TypedRequest<LoginBody>, res: Response) => {
   if (details.empty) {
     return res.status(400).send("Username not found");
   }
+  console.log(details.docs[0].data().salt);
 
-  bcrypt.compare(password, details.docs[0].data().password, (err, result) => {
-    if (err) {
-      return res.status(500).send("Error processing password");
-    }
+  const saltedPassword = password.concat(details.docs[0].data().salt);
+  console.log(saltedPassword);
+  bcrypt.compare(
+    saltedPassword,
+    details.docs[0].data().password,
+    (err, result) => {
+      if (err) {
+        return res.status(500).send("Error processing password");
+      }
 
-    return result
-      ? res.status(201).send("User Successfully Logged In")
-      : res.status(401).send("Incorrect password");
-  });
+      return result
+        ? res.status(201).send("User Successfully Logged In")
+        : res.status(401).send("Incorrect password");
+    },
+  );
 });
 
 // curl -H 'Content-Type: application/json' -d '{ "email": "ben", "color": "pink"}' -X POST http://localhost:3000/subscribe
--- backend/src/requestTypes.ts
+++ backend/src/requestTypes.ts
@@ -2,7 +2,13 @@ export interface TypedRequest<T> extends Express.Request {
   body: T;
 }
 
+export interface RegisterBody {
+  username: string;
+  password: string;
+}
+
 export interface LoginBody {
   username: string;
   password: string;
+  salt: string;
 }
