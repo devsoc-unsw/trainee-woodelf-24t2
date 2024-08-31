0256ac3210c30d672860f74c8fe0b0428b1825cb
Osvaldo Prajitno <z5473276@ad.unsw.edu.au>
2024-08-31 20:59:16 +1000

    small adjustments to dropdown style

--- frontend/src/components/ProfileDropdown/ProfileDropdown.tsx
+++ frontend/src/components/ProfileDropdown/ProfileDropdown.tsx
@@ -1,5 +1,5 @@
 import classes from './ProfileDropdown.module.scss'
-import { Handshake, LogOut, Map, Trophy } from 'lucide-react'
+import { Award, Handshake, LogOut, Map } from 'lucide-react'
 import ProfileDropDownItems from '../ProfileDropdownItems/ProfileDropdownItems'
 
 
@@ -8,17 +8,17 @@ function ProfileDropdown (props: {username: string}) {
     <div className={classes.dropDownMenu}>
       <h3 className={classes.username}>{props.username}</h3>
       
-      <ProfileDropDownItems href='/' text='Unlocked Levels'>
-        <Map color='hsl(52, 100%, 50%)'/>
+      <ProfileDropDownItems href='/' text='Levels'>
+        <Map color='hsl(52, 60%, 50%)' strokeWidth={3}/>
       </ProfileDropDownItems>
       <ProfileDropDownItems href='/' text='Friends'>
-        <Handshake color='hsl(52, 100%, 50%)'/>
+        <Handshake color='hsl(52, 60%, 50%)' strokeWidth={3}/>
       </ProfileDropDownItems>
       <ProfileDropDownItems href='/' text='Achievements'>
-        <Trophy color='hsl(52, 100%, 50%)'/>
+        <Award color='hsl(52, 60%, 50%)' strokeWidth={3}/>
       </ProfileDropDownItems>
       <ProfileDropDownItems href='/' text='Logout'>
-        <LogOut color='hsl(52, 100%, 50%)'/>
+        <LogOut color='hsl(52, 60%, 50%)' strokeWidth={3}/>
       </ProfileDropDownItems>
     </div>
   )
