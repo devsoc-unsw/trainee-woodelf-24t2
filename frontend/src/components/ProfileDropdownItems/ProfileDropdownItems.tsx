cc0955129861bba41db8dcbc7b433661dcda7233
Osvaldo Prajitno <z5473276@ad.unsw.edu.au>
2024-08-31 16:50:04 +1000

    changed position of icon to the right of text

--- frontend/src/components/ProfileDropdownItems/ProfileDropdownItems.tsx
+++ frontend/src/components/ProfileDropdownItems/ProfileDropdownItems.tsx
@@ -4,8 +4,8 @@ import classes from './ProfileDropdownItems.module.scss'
 export default function ProfileDropDownItems (props: {href: string, text: string, children?: React.ReactNode}) {
   return (
     <div className={classes.dropDownItem}>
-      {props.children}
       <a href={props.href} className={classes.dropDownText}>{props.text}</a>
+      {props.children}
     </div>
   )
 }
