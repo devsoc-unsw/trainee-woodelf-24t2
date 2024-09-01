f5452caee0ec66a1dc683471833ba0e26e72db7e
Osvaldo Prajitno <z5473276@ad.unsw.edu.au>
2024-09-02 00:02:12 +1000

    replaced Levels and Friends option with Profile

--- frontend/src/components/ProfileDropdown/ProfileDropdown.tsx
+++ frontend/src/components/ProfileDropdown/ProfileDropdown.tsx
@@ -1,5 +1,5 @@
 import classes from './ProfileDropdown.module.scss'
-import { Award, Handshake, LogOut, Map } from 'lucide-react'
+import { Award, CircleUserRound, LogOut } from 'lucide-react'
 import ProfileDropDownItem from '../ProfileDropdownItem/ProfileDropdownItem'
 
 
@@ -8,17 +8,14 @@ function ProfileDropdown (props: {username: string}) {
     <div className={classes.dropDownMenu}>
       <h3 className={classes.username}>{props.username}</h3>
       
-      <ProfileDropDownItem href='/' text='Levels'>
-        <Map color='hsl(52, 60%, 50%)' strokeWidth={3}/>
-      </ProfileDropDownItem>
-      <ProfileDropDownItem href='/' text='Friends'>
-        <Handshake color='hsl(52, 60%, 50%)' strokeWidth={3}/>
+      <ProfileDropDownItem href='/' text='Profile'>
+        <CircleUserRound color='hsl(52, 70%, 50%)' strokeWidth={3}/>
       </ProfileDropDownItem>
       <ProfileDropDownItem href='/' text='Achievements'>
-        <Award color='hsl(52, 60%, 50%)' strokeWidth={3}/>
+        <Award color='hsl(52, 70%, 50%)' strokeWidth={3}/>
       </ProfileDropDownItem>
       <ProfileDropDownItem href='/' text='Logout'>
-        <LogOut color='hsl(52, 60%, 50%)' strokeWidth={3}/>
+        <LogOut color='hsl(52, 70%, 50%)' strokeWidth={3}/>
       </ProfileDropDownItem>
     </div>
   )
