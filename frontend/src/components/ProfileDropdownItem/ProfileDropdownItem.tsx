af85e276d19bae4955c84244af531c871d741471
Elvene <xleonx0x@users.noreply.github.com>
2024-09-02 16:37:15 +1000

    changed dropdown layout

--- frontend/src/components/ProfileDropdownItem/ProfileDropdownItem.tsx
+++ frontend/src/components/ProfileDropdownItem/ProfileDropdownItem.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import classes from './ProfileDropdownItem.module.scss'
+import React from "react";
+import classes from "./ProfileDropdownItem.module.scss";
 
-export default function ProfileDropDownItems (props: {href: string, text: string, children?: React.ReactNode}) {
+export default function ProfileDropDownItems(props: {
+  href: string;
+  text: string;
+  children?: React.ReactNode;
+}) {
   return (
     <div className={classes.dropDownItem}>
-      <a href={props.href} className={classes.dropDownText}>{props.text}</a>
       {props.children}
+      <a href={props.href} className={classes.dropDownText}>
+        {props.text}
+      </a>
     </div>
-  )
+  );
 }
