df446d6405fc7da13f57404366eebdb0021b2a9d
Elvene <123799877+xleonx0x@users.noreply.github.com>
2024-09-04 02:05:08 +1000

    added mobile responsive navbar

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -6,10 +6,13 @@ import ProfileDropdown from "../ProfileDropdown/ProfileDropdown";
 import { useState } from "react";
 import Credits from "../Credits/Credits";
 import { useNavigate } from "react-router-dom";
-import { Menu } from "lucide-react";
+import { Menu, User, X } from "lucide-react";
+import Sheet from "../Sheet/Sheet";
 
 function Navbar() {
   const [showCredits, setShowCredits] = useState(false);
+  const [showDropDown, setShowDropDown] = useState(false);
+  const [showProfileDropDown, setShowProfileDropDown] = useState(false);
   const navigate = useNavigate();
 
   const toggleCredits = () => {
@@ -20,56 +23,75 @@ function Navbar() {
     }, 10);
   };
 
-  const [showDropDown, setShowDropDown] = useState(false);
-
-  const toggleDropDown = () => {
-    setShowDropDown(!showDropDown);
-  };
-
   return (
-    <nav className={classes.navbar}>
-      <div className={classes.container}>
-          <Menu className={classes.hamburger} />
-        <button
-          className={`${classes.hover} ${classes.logo}`}
-          onClick={() => navigate("/home")}
-        >
-          <Logo size="lg" />
-        </button>
-        <button
-          className={`${classes.hover} ${classes.hideOnMobile}`}
-          onClick={() => navigate("/leaderboard")}
-        >
-          Leaderboard
-        </button>
-        <button
-          className={`${classes.hover} ${classes.hideOnMobile}`}
-          onClick={() => navigate("/gamemodes")}
-        >
-          Gamemodes
-        </button>
-        <button className={`${classes.hover} ${classes.hideOnMobile}`}>
-          Help
-        </button>
-      </div>
-      <div>
-        <button
-          className={`${classes.hover} ${classes.hideOnMobile}`}
-          onClick={toggleCredits}
-        >
-          Credits
-        </button>
-        <div onClick={toggleDropDown} className={classes.hideOnMobile}>
-          <ProfileIcon url="/yellowshirt.svg" />
-          {showDropDown && <ProfileDropdown username="Chris" />}
+    <div>
+      <nav className={classes.navbar}>
+        <div>
+          <button
+            className={`${classes.hover} ${classes.logo}`}
+            onClick={() => navigate("/home")}
+          >
+            <Logo size="lg" />
+          </button>
+          <button
+            className={`${classes.hover} ${classes.hideOnMobile}`}
+            onClick={() => navigate("/leaderboard")}
+          >
+            Leaderboard
+          </button>
+          <button
+            className={`${classes.hover} ${classes.hideOnMobile}`}
+            onClick={() => navigate("/gamemodes")}
+          >
+            Gamemodes
+          </button>
+          <button className={`${classes.hover} ${classes.hideOnMobile}`}>
+            Help
+          </button>
         </div>
-      </div>
-      {showCredits &&
-        createPortal(
-          <Credits onClick={toggleCredits} />,
-          document.getElementById("overlay-root") as HTMLElement,
-        )}
-    </nav>
+
+        <div>
+          <button
+            className={`${classes.hover} ${classes.hideOnMobile}`}
+            onClick={toggleCredits}
+          >
+            Credits
+          </button>
+          <div
+            onClick={() => setShowProfileDropDown(!showProfileDropDown)}
+            className={classes.hideOnMobile}
+          >
+            <ProfileIcon url="/yellowshirt.svg" />
+            {showProfileDropDown && <ProfileDropdown username="Chris" />}
+          </div>
+        </div>
+        {showCredits &&
+          createPortal(
+            <Credits onClick={toggleCredits} />,
+            document.getElementById("overlay-root") as HTMLElement,
+          )}
+        <button
+          className={classes.hamburger}
+          onClick={() => setShowDropDown(!showDropDown)}
+        >
+          {showDropDown ? <X color="white" /> : <Menu color="white" />}
+        </button>
+      </nav>
+      {showDropDown && (
+        <Sheet dropdownNavbar className={classes.sheet}>
+          <button onClick={() => navigate("/leaderboard")}>
+            🏆Leaderboard
+          </button>
+          <button onClick={() => navigate("/gamemodes")}>📌Gamemodes</button>
+          <button onClick={() => navigate("/credits")}>💛Credits</button>
+          <button onClick={() => navigate("/credits")}>
+            <User className={classes.userIcon} color="hsl(52, 100%, 50%)" />
+            Profile
+          </button>
+          <button onClick={() => navigate("/credits")}>😢Logout</button>
+        </Sheet>
+      )}
+    </div>
   );
 }
 
