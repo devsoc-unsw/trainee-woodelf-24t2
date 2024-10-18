263ec1ef4a8792417a1d65c24a51fc6eb5b3aec8
lachlanshoesmith <12870244+lachlanshoesmith@users.noreply.github.com>
2024-10-18 16:27:20 +1100

    🚁 update vite proxy config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
       "/api": {
         target: "https://yellowshirt-backend.fly.dev",
         changeOrigin: true,
+	secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
