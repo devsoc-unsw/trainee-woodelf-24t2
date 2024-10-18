a657cfa75da93a64abd1460e81d565b4f5ccf827
lachlanshoesmith <12870244+lachlanshoesmith@users.noreply.github.com>
2024-10-18 15:27:50 +1100

    🎯 update /api proxy target -> fly endpoint

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://yellowshirt-backend.fly.dev",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
